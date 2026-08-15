b63ff309282332190cd30e9d39de32c823732025
Judy <judysoukkhaphon@gmail.com>
2026-08-15 19:08:33 -0400

    checking dropdown_states

--- frontend/src/Dropdown.jsx
+++ frontend/src/Dropdown.jsx
@@ -2,7 +2,58 @@ import React, {useState, useEffect} from "react";
 import state_abbreviations from "./state_abbreviations"
 import Menu from "./Menu.jsx"
 
-
+states = {
+  "Alabama": "AL",
+  "Alaska": "AK",
+  "Arizona": "AZ",
+  "Arkansas": "AR",
+  "California": "CA",
+  "Colorado": "CO",
+  "Connecticut": "CT",
+  "Delaware": "DE",
+  "Florida": "FL",
+  "Georgia": "GA",
+  "Hawaii": "HI",
+  "Idaho": "ID",
+  "Illinois": "IL",
+  "Indiana": "IN",
+  "Iowa": "IA",
+  "Kansas": "KS",
+  "Kentucky": "KY",
+  "Louisiana": "LA",
+  "Maine": "ME",
+  "Maryland": "MD",
+  "Massachusetts": "MA",
+  "Michigan": "MI",
+  "Minnesota": "MN",
+  "Mississippi": "MS",
+  "Missouri": "MO",
+  "Montana": "MT",
+  "Nebraska": "NE",
+  "Nevada": "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  "Ohio": "OH",
+  "Oklahoma": "OK",
+  "Oregon": "OR",
+  "Pennsylvania": "PA",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  "Tennessee": "TN",
+  "Texas": "TX",
+  "Utah": "UT",
+  "Vermont": "VT",
+  "Virginia": "VA",
+  "Washington": "WA",
+  "West Virginia": "WV",
+  "Wisconsin": "WI",
+  "Wyoming": "WY"
+}
 
 export default function Dropdown(){
     const [stateData, setStateData] = useState();
@@ -22,8 +73,8 @@ export default function Dropdown(){
     }
 
     function dropdown_states(){
-        stateData.map((abbreviation)=>{      
-            console.log(abbreviation)
+        states.map((abbreviation)=>{      
+            <option key={abbreviation} value="">{abbreviation}</option>
         })
     }
     
