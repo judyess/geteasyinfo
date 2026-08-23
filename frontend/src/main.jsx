f844a1d51dc57d1bc9a412ec7b515392565bc839
Judy <judysoukkhaphon@gmail.com>
2026-08-23 15:25:25 -0400

    fixerd import name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import About from "./About.jsx";
 import Menu from "./Menu.jsx";
 import "./App.css";
-import "./Search.jsx";
+import Search from "./Search.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 <BrowserRouter>
