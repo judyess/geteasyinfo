e32e46991b8aa64922dc37ea328461fad55b601b
Judy <judysoukkhaphon@gmail.com>
2026-08-23 18:07:43 -0400

    stripped down the Search comp.

--- frontend/src/Dropdown.jsx
+++ frontend/src/Dropdown.jsx
@@ -1,67 +1,20 @@
 import React, {useState, useEffect} from "react";
 const API_URL = import.meta.env.VITE_API_URL || "";
-const states = {
-  "Alabama": "AL",
-  "Alaska": "AK",
-  "Arizona": "AZ",
-  "Arkansas": "AR",
-  "California": "CA",
-  "Colorado": "CO",
-  "Connecticut": "CT",
-  "Delaware": "DE",
-  "Florida": "FL",
-  "Georgia": "GA",
-  "Hawaii": "HI",
-  "Idaho": "ID",
-  "Illinois": "IL",
-  "Indiana": "IN",
-  "Iowa": "IA",
-  "Kansas": "KS",
-  "Kentucky": "KY",
-  "Louisiana": "LA",
-  "Maine": "ME",
-  "Maryland": "MD",
-  "Massachusetts": "MA",
-  "Michigan": "MI",
-  "Minnesota": "MN",
-  "Mississippi": "MS",
-  "Missouri": "MO",
-  "Montana": "MT",
-  "Nebraska": "NE",
-  "Nevada": "NV",
-  "New Hampshire": "NH",
-  "New Jersey": "NJ",
-  "New Mexico": "NM",
-  "New York": "NY",
-  "North Carolina": "NC",
-  "North Dakota": "ND",
-  "Ohio": "OH",
-  "Oklahoma": "OK",
-  "Oregon": "OR",
-  "Pennsylvania": "PA",
-  "Rhode Island": "RI",
-  "South Carolina": "SC",
-  "South Dakota": "SD",
-  "Tennessee": "TN",
-  "Texas": "TX",
-  "Utah": "UT",
-  "Vermont": "VT",
-  "Virginia": "VA",
-  "Washington": "WA",
-  "West Virginia": "WV",
-  "Wisconsin": "WI",
-  "Wyoming": "WY"
-}
 
 /* expects a function, "func", and a dictionary, "dataset" */
 export default function Dropdown(props){
-    const [dataset, setDataset] = useState([props.dataset])
+    const [dataset, setDataset] = useState(props.dataset)
     const [choice, setChoice] = useState("");
 
     const dropdown_handler = (e)=>{
         const selectedValue = e.target.value; 
         setChoice(selectedValue);
         console.log("Selected option:", selectedValue);
+    }
+
+    const printDataset=()=>{
+        console.log(dataset.entries);
+        console.log(props.dataset);
     }
 
     function send_back_to_caller(e){
@@ -77,12 +30,13 @@ export default function Dropdown(props){
 
     return(
         <div>
+            <button onClick={printDataset}>Print Data</button>
             <form onSubmit={(e) => send_back_to_caller(e)}>
                 <ul>
                     <li>
                         <select value={choice} onChange={dropdown_handler}>
                             <option>--</option>
-                        {Object.entries(states).map(([key, value])=> (
+                        {Object.entries(dataset).map(([key, value])=> (
                             <option key={key}>{value}</option>
                         ))}
                         </select>
--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -2,8 +2,13 @@ import { useEffect, useState } from "react";
 import Dropdown from "./Dropdown"
 import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || "";
-const states = {
-  "Alabama": "AL",
+
+export default function Search(){
+    const [dataItems, setDataItems] = useState();
+    const [state, setState] = useState("")
+
+    const statesList = {
+  "Alabama": "ALabama",
   "Alaska": "AK",
   "Arizona": "AZ",
   "Arkansas": "AR",
@@ -54,68 +59,30 @@ const states = {
   "Wisconsin": "WI",
   "Wyoming": "WY"
 }
-export default function Search(){
-    
-    const [dataItems, setDataItems] = useState();
-    const [type, setType] = useState()
-    const [state, setState] = useState("")
-    const [sessionID, setSessionID] = useState("")
-    const [operation, setOperation] = useState([])
-    const [opsList, setOpsList] = useState([])
 
     useEffect(()=> {
         connect_to_API();
     }, []);
-    
 
     async function connect_to_API() {
         const res = await fetch(`$api/legiscan`);
         console.log("connected")
         const data = await res.json();
-        setOpsList(data)
 }      
 
-    async function getOperation(newIncData) {
-        console.log("REACT getState says: ", newIncData)
-        const response = await axios.put(`${API_URL}/dropdown/${newIncData}`, newIncData)
-        .then((res)=> {console.log("Server response: ", res.data)})
-        .catch((err)=>console.log("error updating getState data: ", err));
-        setOperation(res)
-}
-    const ops_dropdown_handler = (e)=>{
-        const user_choice = e.target.value; 
-        setOperation(user_choice);
-        console.log("Selected option:", operation);
-}
-
     const getState = async (newIncData)=> {
-        console.log("REACT getState says: ", newIncData)
+        console.log("Search.Callback.getState says: ", newIncData)
         const response = await axios.put(`${API_URL}/state/${newIncData}`, newIncData)
         .then((res)=> {console.log("Server response: ", res.data)})
         .catch((err)=>console.log("error updating getState data: ", err));
-        setState(res.data)
+        console.log(response);
 }
-
-    const state_dropdown_handler = (e)=>{
-        const user_choice = e.target.value; 
-        setState(user_choice);
-        console.log("Selected option:", selectedValue);
-}
-
-    async function submitToServer() {
-        const response = await axios.put(`${API_URL}/api/legiscan/submit`, newIncData)
-        .then((res)=> {console.log("Server response: ", res.data)})
-        .catch((err)=>console.log("error updating getDropdown data: ", err));
-    }
-
 
     return(
         <div>
             <div className="app">
                 <label>State </label>
-                <Dropdown func={getState} dataset={null}/>
-                <label>Session ID </label>
-                <button type="submit" onClick={submitToServer}>Submit</button>
+                <Dropdown func={getState} dataset={statesList}/>
             </div>
         </div>
     )
@@ -145,3 +112,17 @@ const operations = [
 ]
 }
 
+/*
+    async function getOperation(newIncData) {
+        console.log("REACT getState says: ", newIncData)
+        const response = await axios.put(`${API_URL}/dropdown/${newIncData}`, newIncData)
+        .then((res)=> {console.log("Server response: ", res.data)})
+        .catch((err)=>console.log("error updating getState data: ", err));
+        setOperation(res)
+}
+    const ops_dropdown_handler = (e)=>{
+        const user_choice = e.target.value; 
+        setOperation(user_choice);
+        console.log("Selected option:", operation);
+}
+*/
